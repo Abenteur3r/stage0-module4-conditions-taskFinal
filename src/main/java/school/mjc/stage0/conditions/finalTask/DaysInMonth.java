8f52ae78d12219093c6c2a552a53ba5afcb5d8be
Ayan <ajan.kohan@gmail.com>
2023-05-26 23:13:35 +0600

    M4 final-task

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,28 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (year>0){
+            switch (month){
+                case(2):
+                    if (year%400==0 || (year%4==0 && year%100 !=0)){
+                       System.out.println(29);
+                    }
+                    else{
+                        System.out.println(28);
+                    }
+                    break;
+                case(6):
+                    System.out.println(30);
+                    break;
+                default:
+                    System.out.println("invalid date");
+                    break;
+            }
+        }
+        else{
+            System.out.println("invalid date");
+        }
+
 
     }
 }
